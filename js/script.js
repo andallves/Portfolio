31aece51386285ac068f3eff7fdab70db2bac30d
Andre Alves <andrealves10a@gmail.com>
2022-12-29 00:22:24 -0300

    Nova funcionalidade do menu hamburguer

--- js/script.js
+++ js/script.js
@@ -1,2 +1,6 @@
-var hamburguer = $('.hamburguer');
+var hamburguer = document.querySelector(".hamburguer");
+
+hamburguer.addEventListener("click", function() {
+    document.querySelector(".container").classList.toggle("show-menu");
+});
 
